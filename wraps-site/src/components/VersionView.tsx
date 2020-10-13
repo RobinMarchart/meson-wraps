62e6c2996d37825cda05fa02bc09e96f9c16cab7
Robin Marchart <robin.marchart@hotmail.com>
2020-10-13 21:49:47 +0200

    fix download file name

--- wraps-site/src/components/VersionView.tsx
+++ wraps-site/src/components/VersionView.tsx
@@ -1,110 +1,113 @@
 import React from 'react';
 import {
-    Box,
-    Paper,
-    Typography,
-    Grid,
-    Button,
-    IconButton,
+  Box,
+  Paper,
+  Typography,
+  Grid,
+  Button,
+  IconButton,
 } from '@material-ui/core';
 import ReactMarkdown from 'react-markdown';
 import { OpenInNew, SentimentVeryDissatisfied } from '@material-ui/icons';
 import { ProjectInfo } from './App';
 import ReadmeViewer from './ReadmeViewer';
-import {ProjectDescrWrapper} from './VersionsView';
+import { ProjectDescrWrapper } from './VersionsView';
 
-export default function VersionView(
-    props: {
-        version: string;
-        project: ProjectInfo;
-    },
-) {
-    return (
-        <Box>
-            <Paper variant="outlined">
-                <Box
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    padding="10px"
-                    marginBottom="30px"
-                    flexDirection="column"
-                >
-                    <Typography variant="h3">
-                        {`${props.project.name} - ${props.version}`}
-                    </Typography>
-                    <Box marginLeft="30px" marginRight="30px">
-                        <ProjectDescrWrapper>
-                            <ReactMarkdown source={props.project.descr} />
-                        </ProjectDescrWrapper>
-                    </Box>
-                </Box>
-            </Paper>
-            <Paper variant="outlined">
-                <Box padding="20px" paddingBottom="30px" marginBottom="30px">
-                    <Grid container spacing={4}>
-                        <Grid item xs={12} md={6}>
-                            <Box display="flex" justifyContent="center" padding="10px">
-                                <Box
-                                    display="flex"
-                                    justifyContent="space-between"
-                                    width="230px"
-                                >
-                                    <Button
-                                        variant="contained"
-                                        color="secondary"
-                                        href={props.project.versions.get(props.version).wrap_url}
-                                        download={`${props.project.name}-${props.version}.wrap`}
-                                    >
-                                        Download Wrap File
-                  </Button>
-                                    <IconButton
-                                        size="small"
-                                        color="secondary"
-                                        href={props.project.versions.get(props.version).wrap_url}
-                                        aria-label={`open wrap file version ${props.version} in new tab`}
-                                    >
-                                        <OpenInNew />
-                                    </IconButton>
-                                </Box>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <Box display="flex" justifyContent="center" padding="10px">
-                                <Button
-                                    variant="contained"
-                                    color="secondary"
-                                    href={props.project.versions.get(props.version).patch_url}
-                                >
-                                    Download Patch
-                </Button>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Paper>
-            <Paper variant="outlined">
-                <Box paddingBottom="30px">
-                    <noscript>
-                        <Box
-                            height="300px"
-                            padding="20px"
-                            display="flex"
-                            justifyContent="space-around"
-                            flexDirection="column"
-                            flexWrap="nowrap"
-                            alignItems="center"
-                            alignContent="center"
-                        >
-                            <Typography variant="h5">Enable JavaScript to show Readme</Typography>
-                            <Box fontSize="120pt">
-                                <SentimentVeryDissatisfied fontSize="inherit" />
-                            </Box>
-                        </Box>
-                    </noscript>
-                    <ReadmeViewer url={props.project.versions.get(props.version).readme.text_url} />
-                </Box>
-            </Paper>
+export default function VersionView(props: {
+  version: string;
+  project: ProjectInfo;
+}) {
+  return (
+    <Box>
+      <Paper variant="outlined">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          padding="10px"
+          marginBottom="30px"
+          flexDirection="column"
+        >
+          <Typography variant="h3">
+            {`${props.project.name} - ${props.version}`}
+          </Typography>
+          <Box marginLeft="30px" marginRight="30px">
+            <ProjectDescrWrapper>
+              <ReactMarkdown source={props.project.descr} />
+            </ProjectDescrWrapper>
+          </Box>
         </Box>
-    );
+      </Paper>
+      <Paper variant="outlined">
+        <Box padding="20px" paddingBottom="30px" marginBottom="30px">
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
+              <Box display="flex" justifyContent="center" padding="10px">
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  width="230px"
+                >
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    href={props.project.versions.get(props.version).wrap_url}
+                    download={`${props.project.name}-${props.version}.wrap`}
+                  >
+                    Download Wrap File
+                  </Button>
+                  <IconButton
+                    size="small"
+                    color="secondary"
+                    href={props.project.versions.get(props.version).wrap_url}
+                    aria-label={`open wrap file version ${props.version} in new tab`}
+                  >
+                    <OpenInNew />
+                  </IconButton>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Box display="flex" justifyContent="center" padding="10px">
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  href={props.project.versions.get(props.version).patch_url}
+                  download={`patch-${props.project.name}-${props.version}.tar.gz`}
+                >
+                  Download Patch
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+      <Paper variant="outlined">
+        <Box paddingBottom="30px">
+          <noscript>
+            <Box
+              height="300px"
+              padding="20px"
+              display="flex"
+              justifyContent="space-around"
+              flexDirection="column"
+              flexWrap="nowrap"
+              alignItems="center"
+              alignContent="center"
+            >
+              <Typography variant="h5">
+                Enable JavaScript to show Readme
+              </Typography>
+              <Box fontSize="120pt">
+                <SentimentVeryDissatisfied fontSize="inherit" />
+              </Box>
+            </Box>
+          </noscript>
+          <ReadmeViewer
+            url={props.project.versions.get(props.version).readme.text_url}
+          />
+        </Box>
+      </Paper>
+    </Box>
+  );
 }
